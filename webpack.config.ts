1eba4d70df29ffb97eee6bb0d5942a7d07bc602a
Ben Coveney <bencoveney@gmail.com>
2020-03-21 13:43:04 +0000

    project set up

---
+++ webpack.config.ts
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsConfigWebpackPlugin = require('ts-config-webpack-plugin');
+const ScssConfigWebpackPlugin = require('scss-config-webpack-plugin');
+const path = require('path');
+const package = require('./package.json');
+
+module.exports = {
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'docs'),
+    },
+    plugins: [
+        new TsConfigWebpackPlugin(),
+        new ScssConfigWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: package.name
+        })
+    ],
+};
